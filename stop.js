ba705e429a7746fd1d5372c969d1ec0353baf23c
sarthaksood9 <sarthaksood9@gmail.com>
2023-08-11 11:45:16 +0530

    chintu dropdown

--- stop.js
+++ stop.js
@@ -1,12 +1,61 @@
 const min = document.getElementById("min");
 const sec = document.getElementById("sec");
+const bg = document.getElementById("mainBody");
+const SBtn = document.getElementById("SBtn");
+const Cbtn = document.getElementById("Cbtn");
 
-// console.log(min.innerHTML);
+// const minSelecter = document.getElementById("minSelect");
+
+
+// var e = document.getElementById("minSelect");
+// function onChange() {
+//   var value = e.value;
+// }
+
+
+// e.onchange = onChange;
+// onChange();
+
+
+function myColor() {
+    var x = document.getElementById("ColorSelect").value;
+    bg.style.backgroundColor=x;
+  }
+function myMin() {
+    var x = document.getElementById("minSelect").value;
+    min.innerHTML=x;
+  }
+function mySec() {
+    var x = document.getElementById("secSelect").value;
+    sec.innerHTML=x;
+  }
+
+
+// console.log(minSelecter.value);
+// // console.log(min.innerHTML);
+// const x=()=>{
+//     minSelecter.value=min.innerHTML;
+// }
+
+let secinter;
 
 const sopFun = () => {
-    var secinter=setInterval(secfunc, 1000);
+    if(SBtn.innerHTML=="Start"){
+        // secinter();
+        secinter=setInterval(secfunc, 1000);
+        SBtn.innerHTML="Stop";
+    }
+    else if(SBtn.innerHTML=="Stop"){
+        SBtn.innerHTML="Start";
+        clearInterval(secinter);
+    }
+
+
+    
+    // minSelecter.value=min.innerHTML;
 
 }
+// clearInterval();
 
 var a=min.innerHTML;
 
@@ -30,8 +79,12 @@ const secfunc = () => {
         
     }
 }
+const Clrfun=()=>{
+    sec.innerHTML=0;
+    min.innerHTML=0;
+    clearInterval(secinter);
+}
 
 
 
-
-window.onload(sopFun());
+// window.onload(sopFun());
